050559fed8e1aadefbfa498394a3d3a21965d9a5
RoanVanQuyen <roan.dev.backend@gmail.com>
2024-09-28 15:51:39 +0700

    Config Application

---
+++ booking-server-command/src/main/java/com/example/bookingservercommand/BookingServerCommandApplication.java
@@ -0,0 +1,13 @@
+package com.example.bookingservercommand;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingServerCommandApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookingServerCommandApplication.class, args);
+	}
+
+}
---
+++ booking-server-command/src/main/java/com/example/bookingservercommand/config/ElasticSearchConfig.java
@@ -0,0 +1,28 @@
+package com.example.bookingservercommand.config;
+
+import org.hibernate.search.backend.elasticsearch.analysis.ElasticsearchAnalysisConfigurationContext;
+import org.hibernate.search.backend.elasticsearch.analysis.ElasticsearchAnalysisConfigurer;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ElasticSearchConfig implements ElasticsearchAnalysisConfigurer {
+    @Override
+    public void configure(ElasticsearchAnalysisConfigurationContext context) {
+        context.analyzer("vietnameseAnalyzer")
+                .custom()
+                .tokenizer("standard")
+                .tokenFilters("lowercase", "asciifolding");
+
+        context.analyzer( "english" ).custom()
+                .tokenizer( "standard" )
+                .tokenFilters( "lowercase", "snowball_english", "asciifolding" );
+
+        context.tokenFilter( "snowball_english" )
+                .type( "snowball" )
+                .param( "language", "English" );
+
+        context.analyzer( "name" ).custom()
+                .tokenizer( "standard" )
+                .tokenFilters( "lowercase", "asciifolding" );
+    }
+}
