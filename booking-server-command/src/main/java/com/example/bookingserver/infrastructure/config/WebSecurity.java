3de11daa2d171d8524c6b17ed16981ff19e7bc2a
RoanVanQuyen <roan.dev.backend@gmail.com>
2024-10-07 11:04:27 +0700

    Config- CORS

---
+++ booking-server-command/src/main/java/com/example/bookingserver/infrastructure/config/WebSecurity.java
@@ -0,0 +1,52 @@
+package com.example.bookingserver.infrastructure.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+@Slf4j
+public class WebSecurity {
+
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http
+                .cors(httpSecurityCorsConfigurer -> corsFilter())
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(
+                        configure ->
+                                configure
+                                        .requestMatchers("/**")
+                                            .permitAll()
+                )
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        return http.build();
+    }
+
+
+    @Bean
+    CorsFilter corsFilter(){
+        CorsConfiguration corsConfiguration = new CorsConfiguration() ;
+        corsConfiguration.addAllowedMethod("*");
+        corsConfiguration.addAllowedOrigin("*");
+        corsConfiguration.addAllowedHeader("*");
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource() ;
+        source.registerCorsConfiguration("/**" , corsConfiguration);
+        return new CorsFilter(source) ;
+    }
+}
