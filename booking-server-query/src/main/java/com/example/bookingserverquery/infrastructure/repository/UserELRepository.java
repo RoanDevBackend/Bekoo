7b9e45edec37758533fb531756ee29c8cf0387c1
RoanVanQuyen <roan.dev.backend@gmail.com>
2024-10-08 15:43:21 +0700

    Feature-Query-User

--- booking-server-query/src/main/java/com/example/bookingserverquery/infrastructure/repository/UserELRepository.java
+++ booking-server-query/src/main/java/com/example/bookingserverquery/infrastructure/repository/UserELRepository.java
@@ -4,10 +4,13 @@ import com.example.bookingserverquery.application.reponse.user.FindByNameRespons
 import com.example.bookingserverquery.domain.User;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.elasticsearch.annotations.Query;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserELRepository extends ElasticsearchRepository<User, String> {
 
+    @Query("{\"match\": {\"name\": {\"query\": \"?0\", \"fuzziness\": \"AUTO\"}}}")
     Page<User> findUsersByName(String name, Pageable pageable);
-
 }
