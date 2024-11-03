dfb6f5ab82f6fc7a6d5fe90d7f721fc35f03e320
RoanVanQuyen <roan.dev.backend@gmail.com>
2024-11-03 23:54:57 +0700

    feat: Update API 'Update Department, Delete Department'

--- booking-server-query/src/main/java/com/example/bookingserverquery/application/event/DepartmentEventHandler.java
+++ booking-server-query/src/main/java/com/example/bookingserverquery/application/event/DepartmentEventHandler.java
@@ -1,8 +1,10 @@
 package com.example.bookingserverquery.application.event;
 
 import com.example.bookingserverquery.domain.Department;
+import com.example.bookingserverquery.domain.Specialize;
 import com.example.bookingserverquery.infrastructure.mapper.DepartmentMapper;
 import com.example.bookingserverquery.infrastructure.repository.DepartmentELRepository;
+import com.example.bookingserverquery.infrastructure.repository.SpecializeELRepository;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import document.constant.TopicConstant;
 import document.event.department.CreateDepartmentEvent;
@@ -10,8 +12,11 @@ import document.event.department.UpdateInfoDepartmentEvent;
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
+
 
 @Component
 @RequiredArgsConstructor
@@ -20,6 +25,7 @@ public class DepartmentEventHandler {
 
     final DepartmentMapper departmentMapper;
     final DepartmentELRepository departmentELRepository;
+    final SpecializeELRepository specializeELRepository;
     final ObjectMapper objectMapper;
 
     @KafkaListener(topics = TopicConstant.DepartmentTopic.CREATE_DEPARTMENT)
@@ -42,6 +48,12 @@ public class DepartmentEventHandler {
         }else {
             departmentMapper.updateInfo(department, updateInfoDepartmentEvent);
             departmentELRepository.save(department);
+
+            Page<Specialize> page= specializeELRepository.findAllByDepartment(department.getId(), Pageable.unpaged());
+            for(Specialize specialize: page.getContent()){
+                specialize.setDepartment(department);
+                specializeELRepository.save(specialize);
+            }
             log.info("UPDATE-INFO-EVENT-SUCCESS: {}", event);
         }
     }
@@ -54,6 +66,8 @@ public class DepartmentEventHandler {
             log.error("ERR-DELETE-DEPARTMENT-EVENT: {}", id);
         }else {
             departmentELRepository.deleteById(id);
+            Page<Specialize> page= specializeELRepository.findAllByDepartment(id, Pageable.unpaged());
+            specializeELRepository.deleteAll(page.getContent());
             log.info("DELETE-DEPARTMENT-EVENT-SUCCESS: {}", id);
         }
     }
--- booking-server-query/src/main/java/com/example/bookingserverquery/infrastructure/repository/SpecializeELRepository.java
+++ booking-server-query/src/main/java/com/example/bookingserverquery/infrastructure/repository/SpecializeELRepository.java
@@ -7,6 +7,8 @@ import org.springframework.data.elasticsearch.annotations.Query;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface SpecializeELRepository extends ElasticsearchRepository<Specialize, String> {
     @Query(value = " {\n" +
