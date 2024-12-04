718f57e2be3473f87463dab726f00e66976b824a
RoanVanQuyen <roan.dev.backend@gmail.com>
2024-12-04 14:54:09 +0700

    feat(main): Auto create ADMIN account.

--- booking-server-command/src/main/java/com/example/bookingserver/BookingServerCommandApplication.java
+++ booking-server-command/src/main/java/com/example/bookingserver/BookingServerCommandApplication.java
@@ -1,30 +1,33 @@
 package com.example.bookingserver;
 
+import com.example.bookingserver.application.command.command.user.CreateUserCommand;
+import com.example.bookingserver.application.command.handle.user.CreateUserHandler;
 import com.example.bookingserver.domain.ERole;
 import com.example.bookingserver.domain.Role;
-import com.example.bookingserver.domain.repository.DoctorRepository;
 import com.example.bookingserver.infrastructure.persistence.repository.RoleJpaRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
+import com.example.bookingserver.infrastructure.persistence.repository.UserJpaRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.HashSet;
+import java.util.Set;
+
+@Slf4j
 @SpringBootApplication
+@RequiredArgsConstructor
 public class BookingServerCommandApplication implements ApplicationRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookingServerCommandApplication.class, args);
 	}
 
-
-
-	@Autowired
-	RoleJpaRepository roleJpaRepository;
-	@Autowired
-	DoctorRepository doctorRepository;
-
+	final RoleJpaRepository roleJpaRepository;
+	final UserJpaRepository userJpaRepository;
+	final CreateUserHandler createUserHandler;
 	@Override
 	public void run(ApplicationArguments args){
 		if(!roleJpaRepository.existsById(ERole.ADMIN.name())) {
@@ -39,9 +42,24 @@ public class BookingServerCommandApplication implements ApplicationRunner {
 			Role role = new Role(ERole.USER);
 			roleJpaRepository.save(role);
 		}
-//		Faker faker= new Faker();
-//		System.out.println(faker.phoneNumber().phoneNumber());
-//		System.out.println(faker.phoneNumber().phoneNumber());
-//		System.out.println(faker.phoneNumber().phoneNumber());
+		if(!userJpaRepository.existsByEmail("bekoo.admin@gmail.com")){
+			CreateUserCommand command = new CreateUserCommand();
+			command.setName("Quản trị hệ thống");
+			command.setCccd("036204018701");
+			command.setEmail("bekoo.admin@gmail.com");
+			command.setPassword("Admin@1234");
+			command.setConfirmPassword("Admin@1234");
+			command.setPhoneNumber("0379715226");
+			command.setProvince("Nam Định");
+			command.setDistrict("Giao Thuỷ");
+			command.setCommune("Giao Long");
+			command.setDob("2004-12-24");
+			command.setGender("Nam");
+			Set<Role> roles = new HashSet<>();
+			roles.add(new Role(ERole.ADMIN));
+			roles.add(new Role(ERole.USER));
+			createUserHandler.execute(command, roles);
+			log.info("Create ADMIN account successfully");
+		}
 	}
 }
--- booking-server-query/src/main/java/com/example/bookingserverquery/BookingServerQueryApplication.java
+++ booking-server-query/src/main/java/com/example/bookingserverquery/BookingServerQueryApplication.java
@@ -1,21 +1,11 @@
 package com.example.bookingserverquery;
 
 
-import com.example.bookingserverquery.infrastructure.repository.DepartmentELRepository;
-import com.example.bookingserverquery.infrastructure.repository.ScheduleELRepository;
-import com.example.bookingserverquery.infrastructure.repository.UserELRepository;
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.info.Contact;
-import io.swagger.v3.oas.annotations.info.Info;
-import io.swagger.v3.oas.annotations.servers.Server;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
+
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 
 
 @SpringBootApplication
@@ -25,10 +15,7 @@ public class BookingServerQueryApplication implements ApplicationRunner {
         SpringApplication.run(BookingServerQueryApplication.class, args);
     }
 
-
-
-
     @Override
-    public void run(ApplicationArguments args) throws Exception {
+    public void run(ApplicationArguments args){
     }
 }
