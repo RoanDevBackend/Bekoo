76e969b292b887f03089d5548939405800c292e6
khuong1742005 <boyzsno1@gmail.com>
2025-04-17 16:00:13 +0700

    feat: get message by JSON

--- booking-server-command/src/main/java/com/example/bookingserver/application/command/handle/chatbot/ChatBotHandler.java
+++ booking-server-command/src/main/java/com/example/bookingserver/application/command/handle/chatbot/ChatBotHandler.java
@@ -1,20 +1,29 @@
 package com.example.bookingserver.application.command.handle.chatbot;
 
 
+import com.example.bookingserver.application.command.reponse.ApiResponse;
+import com.example.bookingserver.application.command.reponse.ChatBotResponse;
 import com.example.bookingserver.application.command.service.ChatBotService;
 import com.example.bookingserver.domain.repository.UserRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
+import lombok.extern.slf4j.Slf4j;
 import org.json.JSONObject;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
 import java.io.IOException;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
@@ -36,6 +45,42 @@ public class ChatBotHandler extends TextWebSocketHandler {
             return;
         }
 
+        if (content.equals("GET MESSAGE")){
+
+            if (!chatBotService.checkUserIdExits(userId)) {
+                ChatBotResponse chatBotResponse = new ChatBotResponse("Xin chào! Tôi có thể giúp gì cho bạn?", 0, LocalDateTime.now());
+                String safeContent = chatBotResponse.getContent().replace("\"", "\\\"");
+                String time = chatBotResponse.getTimestamp().toString();
+                int type = chatBotResponse.getType();
+                String newChatResponse = String.format(
+                        "{\"botResponse\": \"%s\", \"time\": \"%s\", \"type\": %d}",
+                        safeContent, time, type
+                );
+                session.sendMessage(new TextMessage(newChatResponse));
+                System.out.println("Lần đầu sử dụng");
+                return;
+            }
+
+            List<ChatBotResponse> responses = chatBotService.getMessages(chatBotService.takeGroupIdByUserId(userId));
+
+            Map<String, Object> response = new HashMap<>();
+
+            for(ChatBotResponse c : responses){
+                response.put("content", c.getContent());
+                response.put("type", c.getType());
+                response.put("time", c.getTimestamp());
+            }
+
+            ObjectMapper objectMapper = new ObjectMapper();
+            objectMapper.findAndRegisterModules();
+
+            // Convert list to JSON string
+            String jsons = objectMapper.writeValueAsString(response);
+            session.sendMessage(new TextMessage(jsons));
+            System.out.println("Nhận message thành công");
+            return;
+        }
+
         chatBotService.addUserChat(userId, content);
 
         // Check xem AI có khả năng trả lời hay không
@@ -43,8 +88,10 @@ public class ChatBotHandler extends TextWebSocketHandler {
         String botResponse = chatBotService.askAI(content, userId);
         String time = LocalDateTime.now().format(DateTimeFormatter.ISO_LOCAL_DATE_TIME);
         // PARSE TO JSON
-        String botMessage = String.format("{\"botResponse\": \"%s\", \"time\": \"%s\"}", botResponse, time);
+        String safeBotResponse = botResponse.replace("\"", "\\\""); // escape dấu "
+        String botMessage = String.format("{\"botResponse\": \"%s\", \"time\": \"%s\"}", safeBotResponse, time);
         session.sendMessage(new TextMessage(botMessage));
+        System.out.println(content);
     }
 
 //    public void sendMessageToUser(String userId, String message) throws IOException {
